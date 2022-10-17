01cbc033568d5a9ee931c837d1802441d3d8afbf
amicuswat <amicuswat@gmail.com>
2022-10-17 13:31:54 +0300

    Finished get_filename method
    get_filename(url, folder) returns string of folder and filename as os path

--- files_processing.py
+++ files_processing.py
@@ -1,6 +1,7 @@
 import os
 import random
 from urllib.parse import urlparse
+from pathlib import Path
 
 
 def get_file_extention(url):
@@ -12,16 +13,27 @@ def get_file_extention(url):
 
 
 def get_filename(url, folder):
-    # todo - If no Folder create it
-    # todo - Generate file name
+    default_folder = "Files"
+
+    if not folder:
+        folder = default_folder
+
+    Path(folder).mkdir(parents=True, exist_ok=True)
+
+    name_hash = random.getrandbits(128)
+    filename = "%032x" % name_hash
 
     file_extention = get_file_extention(url)
-    filename = f"{file_extention}"
+
+    filename = f"{filename}{file_extention}"
+    filename = Path(folder, filename)
+
     return filename
 
 
 def load_and_save_files(url, folder=None):
     filename = get_filename(url, folder)
+    print(filename)
 
     # todo - Fetch image
     # todo - Save image
