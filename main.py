14a8854269c37c0ccc8291fddabaeddf23b7d48f
amicuswat <amicuswat@gmail.com>
2022-10-20 17:52:04 +0300

    Sending to wall method added

--- main.py
+++ main.py
@@ -70,6 +70,27 @@ def save_photo_on_server(vk_access_token, group_id, upload_response):
     return response.json()['response'][0]
 
 
+def send_comics_to_wall(vk_access_token, group_id, save_response, funny_comment):
+    attachment = f"photo{save_response['owner_id']}_{save_response['id']}"
+
+    params = {
+        'access_token': vk_access_token,
+        'v': '5.131',
+        'owner_id': -int(group_id),
+        'attachments': attachment,
+        'from_group': 1,
+        'message': funny_comment
+    }
+
+    url_with_method = f"{BASE_VK_UPI_URL}" \
+                      f"{VK_METHODS['wall_post']}"
+
+    response = requests.post(url_with_method, params=params)
+    response.raise_for_status()
+
+    return response.json()
+
+
 def load_random_comics():
     response = requests.get(CURRENT_COMMICS_URL)
     response.raise_for_status()
@@ -81,7 +102,7 @@ def load_random_comics():
 
     funny_comment = comics['alt']
 
-    print(funny_comment)
+    return funny_comment
 
 
 def main():
@@ -90,28 +111,22 @@ def main():
 
     group_id = '216541309'
 
+    funny_comment = load_random_comics()
+
     photo_upload_url = get_vk_photos_upload_url(vk_access_token, group_id)
 
     comics_path = get_all_files("Files").pop()
 
     upload_response = upload_photo_to_server(comics_path, photo_upload_url)
 
-    save_response = save_photo_on_server(vk_access_token, group_id, upload_response)
+    save_response = save_photo_on_server(vk_access_token,
+                                         group_id,
+                                         upload_response)
 
-
-
-    print(json.dumps(save_response, indent=2))
-
-    attachments = {
-        'media_id': save_response['id'],
-        'owner_id': save_response['owner_id']
-    }
-    
-
-    print(attachments)
-
-
-
+    send_comics_to_wall(vk_access_token,
+                        group_id,
+                        save_response,
+                        funny_comment)
 
 
 if __name__ == "__main__":
