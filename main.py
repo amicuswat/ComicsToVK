cc6d633e799faf0a78fec695fff9367a994f4f41
amicuswat <amicuswat@gmail.com>
2022-10-24 15:44:35 +0300

    Removed features that were not requested

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
 import random
+from pathlib import Path
+from urllib.parse import urlparse
 
 import requests
 from dotenv import load_dotenv
 
-from files_processing import get_all_files, load_and_save_files
-
 CURRENT_COMMICS_URL = "https://xkcd.com/info.0.json"
 BASE_VK_UPI_URL = 'https://api.vk.com/method/'
+DEFAULT_FOLDER = "Files"
 
 VK_METHODS = {
         'get_groups': 'groups.get',
@@ -106,24 +107,43 @@ def load_random_comics():
     comics = response.json()
 
     img_url = comics['img']
-    load_and_save_files(img_url)
+
+    img_path = save_comics_img(img_url, DEFAULT_FOLDER)
 
     funny_comment = comics['alt']
 
-    return funny_comment
+    return funny_comment, img_path
+
+
+def save_comics_img(url, folder=None):
+    if not folder:
+        folder = DEFAULT_FOLDER
+
+    Path(folder).mkdir(parents=True, exist_ok=True)
+
+    parsed_url =  urlparse(url)
+    filename = os.path.basename(parsed_url.path)
+    file_path = Path(folder, filename)
+
+    response = requests.get(url)
+    response.raise_for_status()
+
+    with open(file_path, "wb") as file:
+        file.write(response.content)
+
+    return file_path
 
 
 def main():
     load_dotenv()
     vk_access_token = os.environ['VK_ACCESS_TOKEN']
 
-    group_id = os.environ['VK_GROUP_ID']''
+    group_id = os.environ['VK_GROUP_ID']
 
-    funny_comment = load_random_comics()
-    comics_path = get_all_files("Files").pop()
+    funny_comment, comics_img_path  = load_random_comics()
 
     photo_upload_url = get_vk_photos_upload_url(vk_access_token, group_id)
-    upload_response = upload_photo_to_server(comics_path, photo_upload_url)
+    upload_response = upload_photo_to_server(comics_img_path, photo_upload_url)
     save_response = save_photo_on_server(vk_access_token,
                                          group_id,
                                          upload_response)
@@ -132,7 +152,7 @@ def main():
                         save_response,
                         funny_comment)
 
-    os.remove(comics_path)
+    os.remove(comics_img_path)
 
 
 if __name__ == "__main__":
